2e2af5c6161915d3e0127afb3ca3d43e99126860
Daniel Ruf <d.ruf@bitexpert.de>
2019-11-05 08:38:06 +0100

    Remove obsolete comments

--- tests/client.js
+++ tests/client.js
@@ -25,8 +25,6 @@ afterAll(() => {
 
 describe('login', () => {
   test('client without any settings emits error', (done) => {
-    // TODO: implement actual check
-    // this outputs only the request object
     const client = new Client(null, null, {
       autoReconnect: false,
       logger: 'noop'
