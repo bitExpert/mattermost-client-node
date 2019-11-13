dca345e037501987d2aa4142cc40639e2373b55c
Daniel Ruf <d.ruf@bitexpert.de>
2019-11-13 22:42:42 +0100

    Generate multiple bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,50 @@
 import typescript from 'rollup-plugin-typescript';
+import resolve from 'rollup-plugin-node-resolve';
+import builtins from 'rollup-plugin-node-builtins';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
 
-export default {
-    input: 'src/client.ts',
-    output: {
-        file: 'dist/bundle.js',
-        format: 'cjs',
+export default [
+    {
+        input: 'src/client.ts',
+        output: {
+            file: 'dist/bundle.esm.js',
+            format: 'esm',
+        },
+        plugins: [
+            typescript(),
+        ],
     },
-    plugins: [
-        typescript(),
-    ],
-};
+    {
+        input: 'src/client.ts',
+        output: {
+            file: 'dist/bundle.cjs.js',
+            format: 'cjs',
+        },
+        plugins: [
+            typescript(),
+        ],
+    },
+    {
+        input: 'src/client.ts',
+        output: {
+            file: 'dist/bundle.umd.js',
+            format: 'umd',
+            name: 'mattermostClient',
+        },
+        plugins: [
+            typescript(),
+            json(),
+            resolve({
+                preferBuiltins: true,
+                browser: true,
+            }),
+            commonjs({
+                include: [
+                    /node_modules/
+                ],
+            }),
+            builtins(),
+        ],
+    },
+];
