be803e0d9022817f14cb2aa5be4a0ba0c0c6e6c5
Daniel Ruf <mac1@daniel-ruf.de>
2019-11-21 20:25:56 +0100

    Use all spec files

--- jest.config.js
+++ jest.config.js
@@ -142,7 +142,7 @@ module.exports = {
 
     // The glob patterns Jest uses to detect test files
     testMatch: [
-        "**/tests/index.spec.js"
+        '**/tests/*.spec.js',
     ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
