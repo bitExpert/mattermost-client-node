d0bf2db0ee6bfa79f59f0bb7ff0e477f852c7e27
Daniel Ruf <d.ruf@bitexpert.de>
2020-01-21 09:32:14 +0100

    Use new testsuite bootstrap

--- tests/bundle.cjs.spec.js
+++ tests/bundle.cjs.spec.js
@@ -1,22 +1,4 @@
+import bootstrap from './testsuite-bootstrap';
 import Client from '../dist/cjs/client';
-import {
-    authenticationTests,
-    channelsTests,
-    initConstants,
-    optionsTests,
-    teamsTests,
-    usersTests,
-    websocketTests,
-} from './testsuite-loader';
 
-initConstants();
-
-// eslint-disable-next-line
-describe('sequentially run cjs bundle tests', () => {
-    authenticationTests(Client);
-    channelsTests(Client);
-    optionsTests(Client);
-    teamsTests(Client);
-    usersTests(Client);
-    websocketTests(Client);
-});
+bootstrap('cjs', Client);
--- tests/bundle.esm.spec.js
+++ tests/bundle.esm.spec.js
@@ -1,22 +1,4 @@
+import bootstrap from './testsuite-bootstrap';
 import Client from '../dist/esm/client';
-import {
-    authenticationTests,
-    channelsTests,
-    initConstants,
-    optionsTests,
-    teamsTests,
-    usersTests,
-    websocketTests,
-} from './testsuite-loader';
 
-initConstants();
-
-// eslint-disable-next-line
-describe('sequentially run esm bundle tests', () => {
-    authenticationTests(Client);
-    channelsTests(Client);
-    optionsTests(Client);
-    teamsTests(Client);
-    usersTests(Client);
-    websocketTests(Client);
-});
+bootstrap('esm', Client);
